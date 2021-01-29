ecd47553eba69c635e89b5888a5aefe39f1fe672
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-29 09:04:12 -0600

    Wherefore art thou coding challenge completed

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 /////////////////////////////////
 // freeCodeCamp JS Challenges //
 ///////////////////////////////
+
+// Challenge 1
+// Wherefore art thou (Make a function that looks through an array of objects and returns an array of all object that have matching property and value pairs.)
+
+const whatIsiNAName = (collection, source) => {
+    let keys = Object.keys(source)
+
+    return collection.filter((obj) => {
+        for(let key of keys) {
+            if(!obj.hasOwnProperty(key) || obj[key] !== source[key]) {
+                return false;
+            }
+        }
+        return true
+    })
+}
+
+console.log(whatIsiNAName([{ first: "Romeo", last: "Montague"}, { first: "Mercuttio", last: null }, { first: "Tybalt", last: "Capulet"}], { last: "Capulet"}));
